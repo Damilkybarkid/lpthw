a6b05b51284bff694b5f5648519731cf0ae56281
Gregor Bell <gregorbell@virginmedia.com>
2018-09-24 12:16:42 +0100

    Added ex2.py, ex3.py and ex3_3.py

---
+++ ex3.py
@@ -0,0 +1,28 @@
+print ("I will now count my chickens:")
+
+# Calculates the number of hens and roosters
+print ("Hens", 25.0 + 30.0 / 6.0)
+print ("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print ("Now I will count the eggs:")
+
+# Calculates the number of eggs
+print (3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print ("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# Determines if 3 + 2 is less than 5 - 7
+print (3.0 + 2.0 < 5.0 - 7.0)
+
+# Calculates 3 + 2 and 5 - 7
+print ("What is 3.0 + 2.0?", 3.0 + 2.0)
+print ("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+
+# Demonstrations of greater than, less than and greater than or equal operators
+print ("Is it greater?", 5.0 > -2.0)
+print ("Is it greater or equal?", 5.0 >= -2.0)
+print ("Is it less or equal?", 5.0 <= -2.0) 
---
+++ ex3_3.py
@@ -0,0 +1,2 @@
+# Calculate 3000000 * 0.92
+print ("What is 3000000 multiplied by 0.92?", 3000000 * 0.92)
